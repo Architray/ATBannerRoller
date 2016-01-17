d238171eddc020f9cb88d92b28e993dcdf16de4d
natary27 <417396038@qq.com>
2016-01-17 15:25:48 +0800

    Create MainFiles

---
+++ ATBannerRoller/ATBannerRoller/ATBannerRollerVC.h
@@ -0,0 +1,13 @@
+//
+//  ATBannerRollerVC.h
+//  ATBannerRoller
+//
+//  Created by Ray on 1/17/16.
+//  Copyright © 2016 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATBannerRollerVC : UIViewController
+
+@end
